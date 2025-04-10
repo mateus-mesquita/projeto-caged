34e00e984175e36d471f9f89d4c6dc53bd6e747d
mateus-mesquita <mateussantoz522@gmail.com>
2025-04-10 15:17:22 -0300

    lista para iterações

---
+++ banco_de_dados/banco_de_dados_caged/listas.R
@@ -0,0 +1,45 @@
+
+# Listas para iterações
+
+# estados
+local = c("","CE","PE")
+
+# sexo
+sexo = c ("", "Feminino", "Masculino", "Não informado")
+
+# raças
+raca_cor = c("", "Branca", "Preta", "Parda", "Amarela", "Indígena", 
+             "Não informada", "Não identificada" )
+# grau de instrução
+grau_de_instrucao = c("", "ANALFABETO", "ATE 5.A INC", "5.A CO FUND", "6.A.9.FUND",
+                      "FUND COMPL", "MEDIO INCOMP", "MEDIO COMPL",
+                      "SUP.INCOMP", "SUP.COMP", "MESTRADO",
+                      "DOUTORADO")
+# seção - cnae
+setores <- c("",
+             "Agricultura, Pecuária, Produção Florestal, Pesca e Aquicultura",
+             "Indústrias Extrativas",
+             "Indústrias de Transformação",
+             "Eletricidade e Gás",
+             "Água, Esgoto, Atividades de Gestão de Resíduos e Descontaminação",
+             "Construção",
+             "Comércio; Reparação de Veículos Automotores e Motocicletas",
+             "Transporte, Armazenagem e Correio",
+             "Alojamento e Alimentação",
+             "Informação e Comunicação",
+             "Atividades Financeiras, de Seguros e Serviços Relacionados",
+             "Atividades Imobiliárias",
+             "Atividades Profissionais, Científicas e Técnicas",
+             "Atividades Administrativas e Serviços Complementares",
+             "Administração Pública, Defesa e Seguridade Social",
+             "Educação",
+             "Saúde Humana e Serviços Sociais",
+             "Artes, Cultura, Esporte e Recreação",
+             "Outras Atividades de Serviços",
+             "Serviços Domésticos",
+             "Organismos Internacionais e Outras Instituições Extraterritoriais",
+             "Não identificado"
+) 
+
+# idades
+idades <- c("","14 a 17","18 a 24","25 a 39","40 a 59","60 ou mais")
