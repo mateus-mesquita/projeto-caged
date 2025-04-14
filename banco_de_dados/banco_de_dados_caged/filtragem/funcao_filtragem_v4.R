a3d4076e75a5eb59506b2927c180ac15830d1213
mateus-mesquita <mateussantoz522@gmail.com>
2025-04-14 20:27:35 -0300

    funções de filtragem para 4 variáveis

---
+++ banco_de_dados/banco_de_dados_caged/filtragem/funcao_filtragem_v4.R
@@ -0,0 +1,39 @@
+# Função para filtragem de registros com 4 variáveis
+filtragem_4V = function(tabela, conexao, local_, sexo_, raca_, idade_, gdi_, setor_) {
+  
+  # Criação da query SQL com CTE (Common Table Expression)
+  query <- glue::glue(
+    "
+    WITH filtragem AS (
+    SELECT *,
+        CASE
+            -- Combinações de 4 variáveis
+            WHEN (uf = '{local_}' AND sexo = '{sexo_}' AND raçacor = '{raca_}' AND faixa_idade = '{idade_}') THEN TRUE
+            WHEN (uf = '{local_}' AND sexo = '{sexo_}' AND raçacor = '{raca_}' AND graudeinstrução = '{gdi_}') THEN TRUE
+            WHEN (uf = '{local_}' AND sexo = '{sexo_}' AND raçacor = '{raca_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (uf = '{local_}' AND sexo = '{sexo_}' AND faixa_idade = '{idade_}' AND graudeinstrução = '{gdi_}') THEN TRUE
+            WHEN (uf = '{local_}' AND sexo = '{sexo_}' AND faixa_idade = '{idade_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (uf = '{local_}' AND sexo = '{sexo_}' AND graudeinstrução = '{gdi_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (uf = '{local_}' AND raçacor = '{raca_}' AND faixa_idade = '{idade_}' AND graudeinstrução = '{gdi_}') THEN TRUE
+            WHEN (uf = '{local_}' AND raçacor = '{raca_}' AND faixa_idade = '{idade_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (uf = '{local_}' AND raçacor = '{raca_}' AND graudeinstrução = '{gdi_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (uf = '{local_}' AND faixa_idade = '{idade_}' AND graudeinstrução = '{gdi_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (sexo = '{sexo_}' AND raçacor = '{raca_}' AND faixa_idade = '{idade_}' AND graudeinstrução = '{gdi_}') THEN TRUE
+            WHEN (sexo = '{sexo_}' AND raçacor = '{raca_}' AND faixa_idade = '{idade_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (sexo = '{sexo_}' AND raçacor = '{raca_}' AND graudeinstrução = '{gdi_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (sexo = '{sexo_}' AND faixa_idade = '{idade_}' AND graudeinstrução = '{gdi_}' AND seção = '{setor_}') THEN TRUE
+            WHEN (raçacor = '{raca_}' AND faixa_idade = '{idade_}' AND graudeinstrução = '{gdi_}' AND seção = '{setor_}') THEN TRUE
+
+            ELSE FALSE
+        END AS validacao
+      FROM {tabela}
+)
+SELECT uf, sexo, raçacor, faixa_idade, graudeinstrução, seção
+FROM filtragem
+WHERE validacao = TRUE;
+    "
+  )
+
+# Executa a query no banco de dados e retorna o resultado
+return(dbGetQuery(conexao, query))
+}
