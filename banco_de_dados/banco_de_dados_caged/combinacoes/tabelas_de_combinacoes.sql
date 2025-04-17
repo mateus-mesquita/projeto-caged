5a0f69dbbfcfbdd654f3f5824b03e33fedec2968
mateus-mesquita <mateussantoz522@gmail.com>
2025-04-17 14:07:51 -0300

    Tabelas de combinações para cada mês

---
+++ banco_de_dados/banco_de_dados_caged/combinacoes/tabelas_de_combinacoes.sql
@@ -0,0 +1,158 @@
+-- Tabela de combinações para cada mês:
+
+-- variáveis:
+
+-- 1) mes
+-- 2) local (estado)
+-- 3) município
+-- 4) sexo
+-- 5) raça
+-- 6) idade
+-- 7) grau de instrução
+-- 8) cnae (seção)
+
+
+-- Tabela para o mês de janeiro:
+CREATE TABLE combinacoes_janeiro (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+)
+
+CREATE TABLE combinacoes_fevereiro (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_marco (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_abril (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_maio (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_junho (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_julho (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_agosto (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_setembro (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_outubro (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_novembro (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
+
+CREATE TABLE combinacoes_dezembro (
+	id INTEGER PRIMARY KEY,
+	mes TEXT,
+	uf TEXT,
+	municipio TEXT,
+	sexo TEXT,
+	raca TEXT,
+	idade TEXT,
+	gdi TEXT,
+	cnae TEXT
+);
